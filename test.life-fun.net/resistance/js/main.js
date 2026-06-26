6383302d21200ea33b365dff0e650de2c9aaa5da
poiopi <sender.works01@gmail.com>
2026-06-26 23:45:39 +0900

    update

--- test.life-fun.net/resistance/js/main.js
+++ test.life-fun.net/resistance/js/main.js
@@ -1,3 +1,4 @@
+import { chapter1 } from './chapters/chapter1.js';
 import { chapter2 } from './chapters/chapter2.js'; 
 import { chapter3 } from './chapters/chapter3.js'; 
 import { chapter4 } from './chapters/chapter4.js'; 
